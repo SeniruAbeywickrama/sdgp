604019293012013e1d197ac9169aa338cdfdbd8d
Coursework <seniru.2019397@iit.ac.lk>
2021-02-02 10:05:21 +0530

    Commit it

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomePageComponent} from './Componants/home-page/home-page.component';
+import {FormPageComponent} from './Componants/form-page/form-page.component';
+import {PriceComponent} from './Componants/price/price.component';
+import {AboutPageComponent} from './Componants/about-page/about-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component : HomePageComponent},
+  {path: 'Form', component :  FormPageComponent},
+  {path: 'Predict' , component: PriceComponent},
+  {path: 'About' , component: AboutPageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {HomePageComponent} from './Componants/home-page/home-page.component';
+import {FormPageComponent} from './Componants/form-page/form-page.component';
+import { PriceComponent } from './Componants/price/price.component';
+import { AboutPageComponent } from './Componants/about-page/about-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    FormPageComponent,
+    PriceComponent,
+    AboutPageComponent
   ],
   imports: [
     BrowserModule,
